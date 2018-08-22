87f7beed4b2ed43ad0fe9ff330da291f48c64549
frettini <2261639M@student.gla.ac.uk>
2018-08-23 01:16:17 +0200

    projectile script and weapon script

---
+++ Assets/Scripts/PlayerBullet.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBullet : MonoBehaviour {
+
+    public float projSpeed = 5f;
+    //public float velY = 0f;
+    Rigidbody2D rb;
+    Transform firePoint;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        firePoint = GameObject.Find("FirePoint").transform;
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        rb.velocity = firePoint.right * projSpeed;
+
+	}
+
+
+    //Destroy projectile upon collision
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name != "Player")
+        {
+            Debug.Log(collision.gameObject.name);
+            Destroy(gameObject);
+        }
+    }
+}
