8d82fbd2fdd3bffa14f0395a03cb6a3e8b481f4f
frettini <2261639M@student.gla.ac.uk>
2018-09-11 16:43:15 +0200

    Replay and Quit button

---
+++ Assets/Scripts/Buttons.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Buttons : MonoBehaviour {
+
+    public void ReplayGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
