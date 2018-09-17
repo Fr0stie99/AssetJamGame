c7a6b17682bc5e6efd1c08d063af9560f8c17326
frettini <2261639M@student.gla.ac.uk>
2018-09-17 02:14:31 +0200

    setting UI for weapon selection

---
+++ Assets/Scripts/UIScripts/WeaponSelectUI.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TeamUtility.IO;
+
+
+public class WeaponSelectUI : MonoBehaviour {
+
+    public PlayerID _playerID, id;
+
+    private void Update()
+    {
+        if(InputManager.GetButtonDown("Shoot1", _playerID))
+        {
+            if(_playerID == PlayerID.One)
+            {
+
+            }
+        }
+    }
+
+    public void BackButton()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex -1);
+    }
+    
+}
